1fafebd0a6398e057704e233bc4de2f83181ffcf
sharayu <sharayubelokar10@gmail.com>
2025-08-04 15:34:50 +0530

    Updated pom.xml and main class for MySQL EC2 deployment

--- src/main/java/TestDBConnection.java
+++ src/main/java/TestDBConnection.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestDBConnection {
-    static final String DB_URL = "jdbc:mysql://localhost:3306/userdb";
-    static final String DB_USER = "root";
-    static final String DB_PASSWORD = "admin"; // replace with your actual password
+    static final String DB_URL = "jdbc:mysql://database-1.cxu6a6k62jkp.ap-south-1.rds.amazonaws.com:3306/database-1";
+    static final String DB_USER = "admin";
+    static final String DB_PASSWORD = "admin123"; // replace with your actual password
 
     public static void main(String[] args) {
         try {
